e9fbf80725823e6ba164581b49e17427a394b550
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-08-30 21:31:38 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,33 +1,24 @@
-// #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int n, temp, result;
+int n, result;
 vector<int> v;
 
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	// freopen("input.txt", "r", stdin);
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> temp;
-		v.push_back(temp);
-	}
+    v.assign(n, 0);
+	for (int i = 0; i < n; i++) cin >> v[i];
 	sort(v.begin(), v.end());
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 0; j < i; j++) {
-			result += v[j];
-		}
-	}
-
+	for (int i = 1; i <= n; i++)
+		for (int j = 0; j < i; j++) result += v[j];	
+    
 	cout << result;
 	return 0;
 }
-
-
