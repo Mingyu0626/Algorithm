70bd39524b026f09959b55aaa99c27cab83d43c2
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-08-30 21:52:40 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -4,21 +4,22 @@
 
 using namespace std;
 
-int n, result;
+int n, sum, result;
 vector<int> v;
-
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	cin >> n;
-    v.assign(n, 0);
+	v.assign(n, 0);
 	for (int i = 0; i < n; i++) cin >> v[i];
 	sort(v.begin(), v.end());
 
-	for (int i = 1; i <= n; i++)
-		for (int j = 0; j < i; j++) result += v[j];	
-    
+	for (int i = 0; i < n; i++) 
+	{
+		sum += v[i];
+		result += sum;
+	}
 	cout << result;
 	return 0;
 }
