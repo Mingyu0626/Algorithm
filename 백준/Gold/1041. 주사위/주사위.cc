195cc32e4e5541059d2b2ddff57c0cbe5ac7ec1f
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-09-05 12:14:06 +0900

    [Gold V] Title: 주사위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1041. 주사위/주사위.cc
@@ -0,0 +1,48 @@
+#include <iostream>>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+long long n, sum, low[3], num[3], maxNum, result;
+pair<long long, long long> d[3];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> n;
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> d[i].first;
+		sum += d[i].first;
+		maxNum = max(maxNum, d[i].first);
+	}
+	for (int i = 2; i >= 0; i--)
+	{
+		cin >> d[i].second;
+		sum += d[i].second;
+		maxNum = max(maxNum, d[i].second);
+	}
+	for (int i = 0; i < 3; i++) low[i] = min(d[i].first, d[i].second);
+	sort(low, low + 3);
+	
+	if (n == 1) result = sum - maxNum;
+	if (n == 2)
+	{
+		result = 4 * (low[0] + low[1]) +
+			4 * (low[0] + low[1] + low[2]);
+	}
+	if (n > 2)
+	{
+		num[0] = (n - 2) * (n - 2) * 5 + (n - 2) * 4;
+		num[1] = 4 + (4 * (n - 2) * 2);
+		num[2] = 4;
+		result = num[0] * low[0] +
+			num[1] * (low[0] + low[1]) +
+			num[2] * (low[0] + low[1] + low[2]);
+	}
+
+	cout << result;
+	return 0;
+}
