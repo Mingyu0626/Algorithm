c46355b4a58a98b0b7b352a138879fa658208571
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-09-19 16:48:12 +0900

    [Gold III] Title: 다리 만들기, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.cc
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-int n, landCount = 1, result = 1e9;
-vector<vector<int>> v;
+int n, landCount, result = 1e9;
+vector<vector<int>> v, dist;
 vector<vector<bool>> visited;
 int dx[4] = { 0, 0, 1, -1 };
 int dy[4] = { 1, -1, 0, 0 };
 
 void bfs1(int x, int y, int lc);
-void bfs2(int x, int y, int lc, int d);
+void bfs2();
 
 int main()
 {
@@ -21,6 +21,7 @@ int main()
     cin.tie(0); cout.tie(0);
     cin >> n;
     v.assign(n, vector<int>(n, 0));
+    dist.assign(n, vector<int>(n, -1));
     visited.assign(n, vector<bool>(n, false));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++) cin >> v[i][j];
@@ -37,19 +38,7 @@ int main()
         }
     }
 
-    fill(visited.begin(), visited.end(), vector<bool>(n, false));
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (!visited[i][j] && v[i][j] != 0)
-            {
-                bfs2(i, j, v[i][j], 0);
-                fill(visited.begin(), visited.end(), vector<bool>(n, false));
-            }
-        }
-    }
+    bfs2();
     cout << result;
     return 0;
 }
@@ -81,28 +70,38 @@ void bfs1(int x, int y, int lc)
     }
 }
 
-void bfs2(int x, int y, int lc, int d)
+void bfs2()
 {
-    queue<pair<pair<int, int>, int>> q;
-    visited[x][y] = true;
-    q.push({ { x, y }, d });
+    queue<pair<int, int>> q;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (v[i][j] != 0)
+            {
+                dist[i][j] = 0;
+                q.push({ i, j });
+            }
+        }
+    }
+
     while (!q.empty())
     {
-        int curX = q.front().first.first;
-        int curY = q.front().first.second;
-        int dist = q.front().second;
+        int curX = q.front().first;
+        int curY = q.front().second;
         q.pop();
         for (int i = 0; i < 4; i++)
         {
-            int newX = curX + dx[i];
-            int newY = curY + dy[i];
-            if (newX < 0 || n <= newX || newY < 0 || n <= newY) continue;
-            if (!visited[newX][newY] && v[newX][newY] == 0)
+            int newx = curX + dx[i];
+            int newy = curY + dy[i];
+            if (newx < 0 || n <= newx || newy < 0 || n <= newy || (v[curX][curY] == v[newx][newy])) continue;
+            if (v[newx][newy] == 0)
             {
-                q.push({ { newX, newY }, dist + 1 });
-                visited[newX][newY] = true;
+                v[newx][newy] = v[curX][curY];
+                dist[newx][newy] = dist[curX][curY] + 1;
+                q.push({ newx, newy });
             }
-            if (v[newX][newY] != 0 && v[newX][newY] != lc) result = min(result, dist);
+            else result = min(result, dist[curX][curY] + dist[newx][newy]);
         }
     }
 }
