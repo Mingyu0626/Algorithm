09d2100bb302cf0b3045e9d4ceb3e989d11a9095
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-10-08 23:16:55 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+long long n;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
+    cout << n * 4;
+}
