901ba07e616ddca1d1d06517fda9081800d5f7cd
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-10-09 11:52:39 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std; 
+
+struct roomInfo
+{
+    long long roomType;
+    long long damage;
+    long long hp;
+};
+
+long long n, playerDamage, playerMaxHP, playerCurHP;
+roomInfo v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> playerDamage;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v.roomType >> v.damage >> v.hp;
+
+        if (v.roomType == 1)
+        {
+            long long numOfPlayerAttack = v.hp / playerDamage;
+            numOfPlayerAttack += (v.hp % playerDamage == 0) ? 0 : 1;
+            long long receivedDamage = (numOfPlayerAttack - 1) * v.damage;
+
+            if (playerCurHP == 0)
+            {
+                playerMaxHP = receivedDamage + 1;
+                playerCurHP = 1;
+            }
+            else
+            {
+                if (playerCurHP > receivedDamage) playerCurHP -= receivedDamage;
+                else
+                {
+                    playerMaxHP = (playerMaxHP - playerCurHP) + receivedDamage + 1;
+                    playerCurHP = 1;
+                }
+            }
+        }
+        else
+        {
+            playerDamage += v.damage;
+            playerCurHP = min(playerMaxHP, playerCurHP + v.hp);
+        }
+    }
+    cout << playerMaxHP;
+}
