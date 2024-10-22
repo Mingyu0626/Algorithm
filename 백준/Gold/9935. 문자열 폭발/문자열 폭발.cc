1e4b656231da12776f8250512abdbc7259e81884
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-10-22 20:48:07 +0900

    [Gold IV] Title: 문자열 폭발, Time: 16 ms, Memory: 7000 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string str, b, result;
+stack<char> s;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> str >> b;
+    for (int i = 0; i < str.size(); i++)
+    {
+        s.push(str[i]);
+        if (str[i] == b[b.size() - 1] && s.size() >= b.size())
+        {
+            string strComp;
+            for (int j = 0; j < b.size(); j++)
+            {
+                strComp += s.top();
+                s.pop();
+            }
+            reverse(strComp.begin(), strComp.end());
+            if (strComp.compare(b) != 0)
+            {
+                for (int j = 0; j < strComp.size(); j++)
+                {
+                    s.push(strComp[j]);
+                }
+            }
+        }
+    }
+    while (!s.empty())
+    {
+        result += s.top();
+        s.pop();
+    }
+    reverse(result.begin(), result.end());
+    if (result.size() == 0) cout << "FRULA";
+    else cout << result;
+    return 0;
+}
