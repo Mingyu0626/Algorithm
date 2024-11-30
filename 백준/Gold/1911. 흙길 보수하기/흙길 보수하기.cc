f4743742a1b734375aa73e11cc159841a41711cb
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-11-30 12:30:08 +0900

    [Gold V] Title: 흙길 보수하기, Time: 4 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.cc
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.cc
@@ -2,29 +2,27 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
+using namespace std; 
 
-int n, l, result;
+int n, l, last, result;
 vector<pair<int, int>> v;
 
 int main()
 {
-	cin >> n >> l;
-	v.assign(n, { 0, 0 });
-	for (int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
-
-	sort(v.begin(), v.end());
-
-	int lastBoard = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (v[i].second <= lastBoard) continue;
-		lastBoard = max(lastBoard, v[i].first);
-		int numOfBoard = (v[i].second - (lastBoard + 1)) / l + 1;
-		result += numOfBoard;
-		lastBoard += l * numOfBoard;
-	}
-
-	cout << result;
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> l;
+    v.assign(n, {0, 0});
+    for (int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i].second <= last) continue;
+        last = max(last, v[i].first);
+        int numOfBoard = (v[i].second - 1 - last) / l + 1;
+        result += numOfBoard;
+        last += numOfBoard * l;
+    }
+    cout << result;
+    return 0;
 }
