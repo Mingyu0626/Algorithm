c8b9fb9f816e7a0879e3c835f540acfc7fff1f81
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-12-05 22:12:45 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -1,30 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int n, result;
-int dp[1001];
-vector<int> v;
+int n, p, dp[1001];
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     cin >> n;
-    v.assign(n + 1, 0);
     for (int i = 1; i <= n; i++)
     {
-        cin >> v[i];
-        dp[i] = v[i];
+        cin >> p;
+        for (int j = i; j <= n; j++)
+        {
+            dp[j] = max(dp[j - i] + p, dp[j]);
+        }
     }
-
-    for (int i = 2; i <= n; i++)
-        for (int j = 1; j < i; j++) dp[i] = max(dp[i], dp[i - j] + v[j]);
-         
     cout << dp[n];
     return 0;
 }
-
-
