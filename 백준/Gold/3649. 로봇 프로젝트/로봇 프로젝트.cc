dcd2bd123f36ea76ba9cdd995b3893bede2fec53
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-07-20 18:03:53 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 996 ms, Memory: 6048 KB -BaekjoonHub

---
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int x, n;
+bool canPlug;
+const int CENTITONANO = 10000000;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    while (cin >> x)
+    {
+        x *= 10000000;
+        canPlug = false;
+        cin >> n;
+        vector<int> v(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+
+        int l = 0, r = n - 1;
+        while (l < r)
+        {
+            int sum = v[l] + v[r];
+            if (sum == x)
+            {
+                cout << "yes " << v[l] << ' ' << v[r] << '\n';
+                canPlug = true;
+                break;
+            }
+            else if (sum < x)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        if (!canPlug)
+        {
+            cout << "danger\n";
+        }
+    }
+}
