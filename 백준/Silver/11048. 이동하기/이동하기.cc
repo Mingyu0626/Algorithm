7550edbcdf5d6edc3f8d629fe31777b6f250973a
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-08-31 10:22:46 +0900

    [Silver II] Title: 이동하기, Time: 64 ms, Memory: 9960 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.cc
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -21,13 +21,6 @@ int main()
         for (int j = 1; j <= m; j++)
         {
             cin >> v[i][j];
-        }
-    }
-
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
             dp[i][j] = max({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] }) + v[i][j];
         }
     }
