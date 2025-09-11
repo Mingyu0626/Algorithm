c34b68f2ee4b1b1f80db3fcf8f9b5475ec6db743
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-09-11 11:23:38 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,57 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <cmath>
+#include <string>
 
 using namespace std;
+template <typename T>
+using Dim2Vec = vector<vector<T>>;
 
 int n;
-vector<vector<char>> v;
+Dim2Vec<char> v;
 
-void recursion(int x, int y, int size);
+void divide(int x, int y, int dist);
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     cin >> n;
-    v.assign(n, vector<char>(n, 0));
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) cin >> v[i][j];
-
-    recursion(0, 0, n);
+    v.assign(n, vector<char>(n, ' '));
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            cin >> v[i][j];
+        }
+    }
+    divide(0, 0, n);
     return 0;
 }
 
-void recursion(int x, int y, int size)
+void divide(int x, int y, int dist)
 {
-
-    char check = v[x][y];
-    bool allSame = true;
-    for (int i = x; i < x + size; i++)
+    char target = v[x][y];
+    for (int i = x; i < x + dist; ++i)
     {
-        for (int j = y; j < y + size; j++)
+        for (int j = y; j < y + dist; ++j)
         {
-            if (check != v[i][j])
+            if (target != v[i][j])
             {
-                allSame = false;
-                i = x + size, j = y + size;
+                cout << '(';
+                divide(x, y, dist / 2);
+                divide(x, y + dist / 2, dist / 2);
+                divide(x + dist / 2, y, dist / 2);
+                divide(x + dist / 2, y + dist / 2, dist / 2);
+                cout << ')';
+                return;
             }
         }
     }
-    if (allSame)
-    {
-        cout << check;
-        return;
-    }
-    else
-    {
-        cout << "(";
-        recursion(x, y, size / 2);
-        recursion(x, y + size / 2, size / 2);
-        recursion(x + size / 2, y, size / 2);
-        recursion(x + size / 2, y + size / 2, size / 2);
-        cout << ")";
-    }
+    cout << target;
 }
