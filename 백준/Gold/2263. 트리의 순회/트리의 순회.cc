09ae722f509c75aac811756b33907991a6f792d6
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-09-12 11:15:08 +0900

    [Gold I] Title: 트리의 순회, Time: 4312 ms, Memory: 8672 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> in, post;
+
+void recursion(int inStartIdx, int inEndIdx, int postStartIdx, int postEndIdx, int root);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
+    in.assign(n + 1, 0), post.assign(n + 1, 0);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> in[i];
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> post[i];
+    }
+    int root = post[n];
+    recursion(1, n, 1, n, root);
+    
+    return 0;
+}
+
+void recursion(int inStartIdx, int inEndIdx, int postStartIdx, int postEndIdx, int root)
+{
+    cout << root << ' ';
+    int rootIdx = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (root == in[i])
+        {
+            rootIdx = i;
+        }
+    }
+
+    int leftSubTreeSize = rootIdx - inStartIdx;
+    int rightSubTreeSize = inEndIdx - rootIdx;
+    int leftPostEndIdx = postStartIdx + leftSubTreeSize - 1;
+    int rightPostEndIdx = postEndIdx - 1;
+    if (0 < leftSubTreeSize)
+    {
+        recursion(inStartIdx, inStartIdx + leftSubTreeSize - 1, postStartIdx, leftPostEndIdx, post[leftPostEndIdx]);
+
+    }
+    if (0 < rightSubTreeSize)
+    {
+        recursion(rootIdx + 1, rootIdx + rightSubTreeSize, leftPostEndIdx + 1, rightPostEndIdx, post[rightPostEndIdx]);
+    }
+}
