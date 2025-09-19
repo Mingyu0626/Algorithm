da17b3d911390905baa4a81c370de082bd9cc8b2
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-09-19 16:56:34 +0900

    [Silver I] Title: 보석 상자, Time: 148 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+long long jealous;
+vector<int> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    v.assign(m, 0);
+    long long l = 1, r = 0, mid;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> v[i];
+        r += v[i];
+    }
+
+    jealous = r;
+    while (l <= r)
+    {
+        mid = (r - l) / 2 + l;
+
+        int children = 0;
+        for (int i = 0; i < m; ++i)
+        {
+            children += v[i] / mid;
+            children += (v[i] % mid != 0) ? 1 : 0;
+        }
+
+        if (children <= n)
+        {
+            jealous = min(jealous, mid);
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    cout << jealous;
+    return 0;
+}
