33e0d8558fcede209ed9fa6472ae1d8ef8494d7e
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-09-22 10:01:27 +0900

    [Silver II] Title: 파닭파닭, Time: 368 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long s, c, l, r, m, maximumLength;
+long long result, sum;
+vector<long long> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> s >> c;
+    v.assign(s, 0);
+    l = 1;
+    for (int i = 0; i < s; ++i)
+    {
+        cin >> v[i];
+        r = max(r, v[i]);
+        sum += v[i];
+    }
+
+    while (l <= r)
+    {
+        m = (r - l) / 2 + l;
+        long long cnt = 0;
+        for (int i = 0; i < s; ++i)
+        {
+            cnt += v[i] / m;
+        }
+        if (c <= cnt)
+        {
+            l = m + 1;
+            maximumLength = max(maximumLength, m);
+        }
+        else
+        {
+            r = m - 1;
+        }
+    }
+    
+    result = sum - (maximumLength * c);
+    cout << result;
+    return 0;
+}
