2ad54e3e8654b794352cf5c536aa220fc5571507
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-09-23 14:24:38 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+long long t, x, y, diff;
+vector<long long> v(1, 0);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> t;
+    long long moveable = 0, cnt = 0;
+    while (moveable < INT_MAX)
+    {
+        moveable += cnt / 2 + 1;
+        v.emplace_back(moveable);
+        cnt++;
+    }
+    while (cin >> x >> y)
+    {
+        diff = y - x;
+        cout << lower_bound(v.begin(), v.end(), diff) - v.begin() << '\n';
+    }
+    return 0;
+}
