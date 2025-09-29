e9c4185f7d3916fd65c7e73e7faab321a7f57a28
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-09-29 16:51:42 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int k;
+vector<int> v;
+
+void combination(int idx, vector<int>& result);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    while (cin >> k && k != 0)
+    {
+        v.assign(k + 1, 0);
+        for (int i = 1; i <= k; ++i)
+        {
+            cin >> v[i];
+        }
+        vector<int> result;
+        combination(0, result);
+        cout << '\n';
+    }
+    return 0;
+}
+
+void combination(int idx, vector<int>& result)
+{
+    if (result.size() == 6)
+    {
+        for (const auto& a : result)
+        {
+            cout << a << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    for (int i = idx + 1; i <= k; ++i)
+    {
+        result.emplace_back(v[i]);
+        combination(i, result);
+        result.pop_back();
+    }
+}
