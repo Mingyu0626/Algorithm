85f3b616782a8b2ccccaf853e1d47924769b0fdd
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-10-10 16:08:50 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int t, n, m;
+vector<int> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> t;
+    while (cin >> n)
+    {
+        v.assign(n, 0);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> v[i];
+        }
+        cin >> m;
+        vector<int> dp(m + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = v[i]; j <= m; ++j)
+            {
+                dp[j] += dp[j - v[i]];
+            }
+        }
+        cout << dp[m] << '\n';
+    }
+    return 0;
+}
