0a760bd8a2943318fe9b61594965da6d56e54ed9
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-11-18 09:41:38 +0900

    [level 2] Title: 구명보트, Time: 1.77 ms, Memory: 4.68 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,48 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
-#include <string_view>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <list>
-#include <ctime>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 
 using namespace std;
 
 int solution(vector<int> v, int limit) 
 {
-    int n = v.size();
+    int n = v.size(), l = 0, r = n - 1;
     sort(v.begin(), v.end());
-    int l = 0, r = n - 1;
-    int curSum = v[r], result = 1;
-    while (l < r)
+    int curSum = 0, result = 0;
+    while (l <= r)
     {
+        curSum = v[r], result++;
         if (v[l] + curSum <= limit)
         {
             l++;
         }
         r--;
-        if (l <= r)
-        {
-            result++;
-            curSum = v[r];
-        }
     }
     return result;
 }
