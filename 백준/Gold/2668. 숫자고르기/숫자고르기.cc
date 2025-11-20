554c25dd4b283c104cedf535db26c012a01cddde
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-11-20 16:21:51 +0900

    [Gold V] Title: 숫자고르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<int> v, result;
+vector<bool> visited;
+
+bool dfs(int startIdx, int curIdx, int curDepth);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
+    v.assign(n + 1, 0), visited.assign(n + 1, false);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> v[i];
+    }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        if (!visited[i])
+        {
+            dfs(i, i, 1);
+        }
+    }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        if (visited[i])
+        {
+            result.emplace_back(i);
+        }
+    }
+    cout << result.size() << '\n';
+    for (const int& num : result)
+    {
+        cout << num << '\n';
+    }
+    return 0;
+}
+
+bool dfs(int startIdx, int curIdx, int curDepth)
+{
+    visited[curIdx] = true;
+    if (v[curIdx] == startIdx)
+    {
+        return true;
+    }
+    if (!visited[v[curIdx]] && dfs(startIdx, v[curIdx], curDepth + 1))
+    {
+        return true;
+    }
+    visited[curIdx] = false;
+    return false;
+}
