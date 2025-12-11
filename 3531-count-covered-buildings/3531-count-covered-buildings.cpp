edd605c1e3b22f3736e3cace51576453d9bcd743
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-12-11 09:31:34 +0900

    Time: 28 ms (85.64%), Space: 283.9 MB (85.64%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.cpp
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        vector<set<int>> r(n + 1), c(n + 1);
-        for (int i = 0; i < buildings.size(); ++i) {
-            int row = buildings[i][0], col = buildings[i][1];
-            r[row].insert(col), c[col].insert(row);
+        vector<int> maxRow(n + 1), minRow(n + 1, n + 1);
+        vector<int> maxCol(n + 1), minCol(n + 1, n + 1);
+
+        for (const auto& p : buildings) {
+            int x = p[0], y = p[1];
+            maxRow[y] = max(maxRow[y], x);
+            minRow[y] = min(minRow[y], x);
+            maxCol[x] = max(maxCol[x], y);
+            minCol[x] = min(minCol[x], y);
         }
 
         int result = 0;
-        for (int i = 0; i < buildings.size(); ++i) {
-            int row = buildings[i][0], col = buildings[i][1];
-            if (r[row].upper_bound(col) == r[row].end() || 
-                r[row].lower_bound(col) == r[row].begin() ||
-                c[col].upper_bound(row) == c[col].end() || 
-                c[col].lower_bound(row) == c[col].begin()) {
-                    continue;
-                }
+        for (const auto& p : buildings) {
+            int x = p[0], y = p[1];
+            if (x <= minRow[y] || maxRow[y] <= x || y <= minCol[x] || maxCol[x] <= y) {
+                continue;
+            } 
             result++;
         }
         return result;
