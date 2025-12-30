311ac6a792ea7e75873e7f68ebe1d58be7c991f2
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-12-30 10:39:32 +0900

    Refactor secret code decryption logic
    Refactor code to use unordered_set for combinations and validation.

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
@@ -1,25 +1,37 @@
-#include <iostream>
 #include <vector>
 #include <unordered_set>
 
 using namespace std;
 
-vector<vector<int>> q;
-vector<int> ans;
 int m, n;
+vector<unordered_set<int>> q;
+vector<int> ans;
 
-void makeCombination(vector<int> cur, int& result, int maxNum);
+void initHashSet(const vector<vector<int>>& Q);
+void makeCombination(vector<int>& cur, int& result, int maxNum);
 bool validation(const vector<int>& cur);
 
-
 int solution(int N, vector<vector<int>> Q, vector<int> Ans) {
-    q = Q, ans = Ans, m = q.size(), n = N;
+    m = Q.size(), n = N, ans = Ans;
+    initHashSet(Q);
     int result = 0;
-    makeCombination(vector<int>(), result, 0);
+    vector<int> cur;
+    makeCombination(cur, result, 0);
     return result;
 }
 
-void makeCombination(vector<int> cur, int& result, int maxNum) {
+void initHashSet(const vector<vector<int>>& Q) {
+    unordered_set<int> us;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            us.insert(Q[i][j]);
+        }
+        q.push_back(us);
+        us.clear();
+    }
+}
+
+void makeCombination(vector<int>& cur, int& result, int maxNum) {
     if (cur.size() == 5) {
         result += (validation(cur)) ? 1 : 0;
         return;
@@ -32,15 +44,10 @@ void makeCombination(vector<int> cur, int& result, int maxNum) {
 }
 
 bool validation(const vector<int>& cur) {
-    unordered_set<int> us;
-    for (const auto& num : cur) {
-        us.insert(num);
-    }
-    
     for (int i = 0; i < m; ++i) {
         int sameNum = 0;
         for (int j = 0; j < 5; ++j) {
-            sameNum += (us.find(q[i][j]) != us.end()) ? 1 : 0;
+            sameNum += (q[i].find(cur[j]) != q[i].end()) ? 1 : 0;
         }
         if (sameNum != ans[i]) {
             return false;
