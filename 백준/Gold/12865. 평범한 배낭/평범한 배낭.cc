d41e5ccbcd889315570c7f46b998af3bbe875ab7
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-01-14 11:34:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,32 +1,22 @@
-// #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int n, k;
-pair<int, int> arr[101] = { {0, 0}, }; // weight, value
-int dp[101][100001];
+int n, k, w, v;
+vector<int> dp;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	// freopen("input.txt", "r", stdin);
-	cin >> n >> k;
-
-	for (int i = 1; i <= n; i++) 
-		cin >> arr[i].first >> arr[i].second;
-	
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (j - arr[i].first >= 0)
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - arr[i].first] + arr[i].second);
-			else
-				dp[i][j] = dp[i - 1][j];
-		}
-	}
-
-	cout << dp[n][k];
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> k;
+    dp.assign(k + 1, 0);
+    for (int i = 0; i < n; ++i) {
+        cin >> w >> v;
+        for (int j = k; j >= 0; --j) {
+            dp[j] = (w <= j) ? max(dp[j], dp[j - w] + v) : dp[j];
+        }
+    }
+    cout << dp[k];
+    return 0;
 }
