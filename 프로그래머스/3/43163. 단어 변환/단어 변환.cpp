5a0bbbebfe2f86a784385365ea562e612f49b48e
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-01-14 10:58:38 +0900

    [level 3] Title: 단어 변환, Time: 0.09 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <iostream>
 #include <algorithm>
 #include <unordered_set>
@@ -9,19 +8,16 @@ using namespace std;
 
 int bfs(const string begin, const string target, const vector<string>& words);
 
-int solution(string begin, string target, vector<string> words) 
-{
+int solution(string begin, string target, vector<string> words) {
     return bfs(begin, target, words);
 }
 
-int bfs(const string begin, const string target, const vector<string>& words)
-{
+int bfs(const string begin, const string target, const vector<string>& words) {
     unordered_set<string> visited;
     visited.insert(begin);
     
     unordered_set<string> wordHashSet;
-    for (string word : words)
-    {
+    for (const auto& word : words) {
         wordHashSet.insert(word);
     }
     
@@ -29,29 +25,23 @@ int bfs(const string begin, const string target, const vector<string>& words)
     q.push(begin);
     
     int level = 0;
-    while (!q.empty())
-    {
+    while (!q.empty()) {
         int qSize = q.size();
-        for (int i = 0; i < qSize; ++i)
-        {
+        for (int i = 0; i < qSize; ++i) {
             string curWord = q.front();
             q.pop();
             
-            if (curWord == target)
-            {
+            if (curWord == target) {
                 return level;
             }
             
-            for (int j = 0; j < curWord.length(); ++j)
-            {
+            for (int j = 0; j < curWord.length(); ++j) {
                 string newWord = curWord;
                 char newChar = 'a';
-                for (int k = 0; k <= 25; ++k)
-                {
+                for (int k = 0; k <= 25; ++k) {
                     newWord[j] = newChar;
-                    if (wordHashSet.find(newWord) != wordHashSet.end() &&
-                       visited.find(newWord) == visited.end())
-                    {
+                    if (wordHashSet.find(newWord) != wordHashSet.end() && 
+                        visited.find(newWord) == visited.end()) {
                         q.push(newWord);
                         visited.insert(newWord);
                     }
