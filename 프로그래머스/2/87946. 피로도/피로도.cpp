80f69f2242faffaeb7c1c474d805cd08a5a5e1c2
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-01-17 17:15:58 +0900

    [level 2] Title: 피로도, Time: 0.46 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,27 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
-#include <string_view>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <list>
-#include <ctime>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 
 using namespace std;
 
