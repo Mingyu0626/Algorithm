f843a35e6509bbc6b41c4e24fe466457dab22e1d
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-02-13 09:42:00 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 433.15 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -1,68 +1,52 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
-using pii = pair<int, int>;
-
 template <typename T>
 using Dim2Vec = vector<vector<T>>;
+using pii = pair<int, int>;
 
+int n;
 Dim2Vec<int> v(1000001, vector<int>());
+vector<int> result(4, 0);
 
-void dfs(int startNode, int curNode, int numOfVisitEdges, vector<int>& result);
+void dfs(int startNode, int curNode, int numOfVisitEdges);
 
-vector<int> solution(vector<vector<int>> edges) 
-{
-    vector<int> result(4, 0);
-    int n = edges.size();
-    unordered_map<int, pii> um;
-    pair<int, pii> createNodeInfo = {0, {0, 0}}; // {NodeNum, {NumOfIndegree, NumOfOutdegree}}
-    for (int i = 0; i < n; ++i)
-    {
-        um[edges[i][0]].second++;
-        um[edges[i][1]].first++;
+vector<int> solution(vector<vector<int>> edges) {
+    n = edges.size();
+    unordered_map<int, pii> nodeInfos; // {NodeNum, {NumOfIndegree, NumOfOutdegree}}
+    pair<int, pii> createNodeInfo = {0, {0, 0}};
+    for (int i = 0; i < n; ++i) {
+        nodeInfos[edges[i][0]].second++;
+        nodeInfos[edges[i][1]].first++;
         v[edges[i][0]].emplace_back(edges[i][1]);
     }
     
-    for (const auto& a : um)
-    {
-        if (a.second.first == 0 && createNodeInfo.second.second < a.second.second)
-        {
+    for (const auto& a : nodeInfos) {
+        if (a.second.first == 0 && createNodeInfo.second.second < a.second.second) {
             createNodeInfo = a;
         }
     }
 
-    result[0] = createNodeInfo.first;
-    for (int i = 0; i < v[result[0]].size(); ++i)
-    {
-        dfs(v[result[0]][i], v[result[0]][i], 0, result);
+    int createNodeNum = result[0] = createNodeInfo.first;
+    for (int i = 0; i < v[createNodeNum].size(); ++i) {
+        dfs(v[createNodeNum][i], v[createNodeNum][i], 0);
     }
-    
     return result;
 }
 
-void dfs(int startNode, int curNode, int numOfVisitEdges, vector<int>& result)
-{
-    if (startNode == curNode && 0 < numOfVisitEdges)
-    {
+void dfs(int startNode, int curNode, int numOfVisitEdges) {
+    if (startNode == curNode && 0 < numOfVisitEdges) {
         result[1]++;
         return;
     }
-    
-    if (v[curNode].size() == 0)
-    {
+    if (v[curNode].size() == 0) {
         result[2]++;
         return;
     }
-    
-    if (1 < v[curNode].size())
-    {
+    if (1 < v[curNode].size()) {
         result[3]++;
         return;
     }
-    
-    dfs(startNode, v[curNode][0], numOfVisitEdges + 1, result);
+    dfs(startNode, v[curNode][0], numOfVisitEdges + 1);
 }
