cf2e01931d77ba2015683e774ec4627477d2b1be
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-03-31 10:51:49 +0900

    Easy 2946-matrix-similarity-after-cyclic-shifts 03-31-2026 0 ms (100%) 31.2 MB (89.09%)

--- LeetCode/Easy/2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ LeetCode/Easy/2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -2,29 +2,14 @@ class Solution {
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
         int m = mat.size(), n = mat[0].size();
-        deque<deque<int>> dq(m, deque<int>(n, 0));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                dq[i][j] = mat[i][j];
-            }
-        }
+        k %= n;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-        while (0 < k--) {
-            for (int i = 0; i < m; ++i) {
-                if (i % 2 == 0) {
-                    int f = dq[i].front(); dq[i].pop_front();
-                    dq[i].push_back(f);
+                if (i % 2 == 0 && mat[i][j] != mat[i][(j + k) % n]) {
+                    return false;
                 }
-                else {
-                    int b = dq[i].back(); dq[i].pop_back();
-                    dq[i].push_front(b);
-                }
-            }
-        }
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (dq[i][j] != mat[i][j]) {
+                if (i % 2 != 0 && mat[i][j] != mat[i][(j - k + n) % n]) {
                     return false;
                 }
             }
