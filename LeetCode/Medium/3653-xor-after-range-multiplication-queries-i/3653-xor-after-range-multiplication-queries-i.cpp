8790e91e85d42aa266efb64091363f73149958ce
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-04-08 09:26:37 +0900

    04-08-2026 3653-xor-after-range-multiplication-queries-i 59 ms (68.9%) 164.6 MB (26.32%)

---
+++ LeetCode/Medium/3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& n, vector<vector<int>>& q) {
+        vector<long long> nums;
+        for (const auto& a : n) {
+            nums.emplace_back(a);
+        }
+
+        const int MOD = 1000000007;
+        for (int i = 0; i < q.size(); ++i) {
+            int l = q[i][0], r = q[i][1], k = q[i][2], v = q[i][3];
+            for (int j = l; j <= r; j += k) {
+                long long update = (nums[j] * v) % MOD;
+                nums[j] = (int)update;
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
