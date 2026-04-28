5d93b8ba2602213c0adfc03c5d74a4d488119c3c
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-04-28 10:50:49 +0900

    Medium 2033-minimum-operations-to-make-a-uni-value-grid 04-28-2026 N/A (0%) N/A (0%)

--- LeetCode/Medium/2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ LeetCode/Medium/2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid[0].size(), r = grid[0][0] % x;
         vector<int> v;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
+                if (r != grid[i][j] % x) {
+                    return -1;
+                }
                 v.emplace_back(grid[i][j]);
             }
         }
         sort(v.begin(), v.end());
-
-        int r = v[0] % x;
-        for (int i = 0; i < m * n; ++i) {
-            if (r != v[i] % x) {
-                return -1;
-            }
-        }
 
         int midIdx = m * n / 2, target = v[midIdx], result = 0;
         for (int i = 0; i < m * n; ++i) {
