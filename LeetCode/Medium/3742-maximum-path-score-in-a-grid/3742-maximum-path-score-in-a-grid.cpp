95ce2fa496051a7b10c36312ad61f1a27bb7e75f
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-04-30 10:02:51 +0900

    Medium 3742-maximum-path-score-in-a-grid 04-30-2026 N/A (0%) N/A (0%)

--- LeetCode/Medium/3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ LeetCode/Medium/3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -7,29 +7,39 @@ public:
         vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n + 1, vector<int>(1001, -1)));
 
         if (grid[0][0] == 0) {
-            dp[1][1][0] = 0;
+            dp[0][0][0] = 0;
         }
         else {
-            dp[1][1][1] = grid[0][0];
+            dp[0][0][1] = grid[0][0];
         }
 
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                for (int c = 0; c < i + j; ++c) {
-                    int prev = max(dp[i - 1][j][c], dp[i][j - 1][c]);
-                    if (prev == -1) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int c = 0; c <= i + j; ++c) {
+                    if (dp[i][j][c] == -1) {
                         continue;
                     }
-                    int cost = grid[i - 1][j - 1] == 0 ? 0 : 1;
-                    int score = grid[i - 1][j - 1];
-                    dp[i][j][c + cost] = prev + score;
+
+                    if (j + 1 < n) {
+                        int newCost = c + (grid[i][j + 1] == 0 ? 0 : 1);
+                        if (newCost <= k){
+                            dp[i][j + 1][newCost] = max(dp[i][j + 1][newCost], dp[i][j][c] + grid[i][j + 1]);
+                        }
+                    }
+
+                    if (i + 1 < m) {
+                        int newCost = c + (grid[i + 1][j] == 0 ? 0 : 1);
+                        if (newCost <= k) {
+                            dp[i + 1][j][newCost] = max(dp[i + 1][j][newCost], dp[i][j][c] + grid[i + 1][j]);
+                        }
+                    }
                 }
             }
         }
 
         int result = -1;
         for (int i = 0; i <= k; ++i) {
-            result = max(result, dp[m][n][i]);
+            result = max(result, dp[m - 1][n - 1][i]);
         }
         return result;
     }
