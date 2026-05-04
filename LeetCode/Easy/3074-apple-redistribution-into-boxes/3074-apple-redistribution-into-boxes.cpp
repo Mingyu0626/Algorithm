6acefe222abb137b53b9c081c5fcffde50ee5839
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-05-04 14:48:35 +0900

    Easy 3074-apple-redistribution-into-boxes 05-04-2026 6 ms (1.18%) 32.9 MB (95.15%)

---
+++ LeetCode/Easy/3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int sum = 0;
+        for (int i = 0; i < apple.size(); ++i) {
+            sum += apple[i];
+        }   
+        sort(capacity.begin(), capacity.end(), greater<int>());
+        for (int i = 0; i < capacity.size(); ++i) {
+            sum -= capacity[i];
+            if (sum <= 0) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+};
