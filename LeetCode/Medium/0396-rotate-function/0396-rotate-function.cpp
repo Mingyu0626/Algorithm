a7da7491e41917b27d8a37b1cde8d02d82844a95
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-05-07 09:38:56 +0900

    Medium 0396-rotate-function 05-07-2026 0 ms (100%) 99.5 MB (97.75%)

--- LeetCode/Medium/0396-rotate-function/0396-rotate-function.cpp
+++ LeetCode/Medium/0396-rotate-function/0396-rotate-function.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
-        int n = nums.size(), sum = 0, result = 0;
-        vector<int> dp(n, 0);
+        int n = nums.size(), sum = 0, lastSum = 0, result = 0;
         for (int i = 0; i < n; ++i) {
-            dp[0] += i * nums[i];
+            lastSum += i * nums[i];
             sum += nums[i];
         }
         
-        result = dp[0];
+        result = lastSum;
         for (int i = 1; i < n; ++i) {
-            dp[i] = dp[i - 1] + sum - (n * nums[n - i]);
-            result = max(result, dp[i]);
+            lastSum += sum - (n * nums[n - i]);
+            result = max(result, lastSum);
         }
         return result;
     }
