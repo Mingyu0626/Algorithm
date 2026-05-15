69a8c91df9d3422964f8bd2eb9ebc63cda722136
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-05-15 14:18:17 +0900

    Medium 0334-increasing-triplet-subsequence 05-15-2026 3 ms (42.05%) 115.7 MB (66.96%)

--- LeetCode/Medium/0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ LeetCode/Medium/0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        vector<int> lis;
-        lis.emplace_back(nums[0]);
-        for (int i = 0; i < nums.size(); ++i) {
-            int cur = nums[i];
-            if (lis.back() < cur) {
-                lis.emplace_back(cur);
+        int i = INT_MAX, j = INT_MAX;
+        for (const int& a : nums) {
+            if (a <= i) {
+                i = a;
+            }
+            else if (a <= j) {
+                j = a;
             }
             else {
-                int idx = lower_bound(lis.begin(), lis.end(), cur) - lis.begin();
-                lis[idx] = cur;
+                return true;
             }
         }
-        return 3 <= lis.size();
+        return false;
     }
 };
