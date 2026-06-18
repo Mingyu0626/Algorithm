28ae6b0ad5ef96d74530bcfedbb600607ce4a026
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2026-06-18 10:50:08 +0900

    Time: 0 ms (100%), Space: 8.3 MB (0%) - LeetHub

--- LeetCode/Medium/1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ LeetCode/Medium/1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
         double hourHand = 30.0 * hour + 0.5 * minutes;
-        double minuteHand = 6 * minutes;
+        double minuteHand = 6.0 * minutes;
         double diff = abs(hourHand - minuteHand);
         return min(360.0 - diff, diff);
     }
